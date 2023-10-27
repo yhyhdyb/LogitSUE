7dbdeae97e2201af6a93ccf3d12a8eaa4ee2347e
29303 <2930387428@qq.com>
2023-10-27 18:53:10 +0800

    123

---
+++ urban/urban/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace WT_LogitSUE
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                CNetwork Network = new CNetwork();
+
+                // 读取文件
+                Network.ReadNode("C:\\Users\\29303\\Desktop\\urban\\LogitSUE\\Node_SiouxFalls.txt");
+                Network.ReadLink("C:\\Users\\29303\\Desktop\\urban\\LogitSUE\\Link_SiouxFalls.txt");
+                Network.ReadODpairs("C:\\Users\\29303\\Desktop\\urban\\LogitSUE\\ODPairs_SiouxFalls.txt");
+                Network.ReadPath("C:\\Users\\29303\\Desktop\\urban\\LogitSUE\\Path_SiouxFalls.txt");
+
+                // 运行求解算法
+                Network.LogitSUE();
+                // 显示结果
+                Console.ReadKey();
+            }
+        }
+    }
+
